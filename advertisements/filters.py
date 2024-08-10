e6da3c0f969f93cd850b2e9cf44030a90990d15e
Alex Legkovsky <legkovsky.alexei@yandex.ru>
2024-08-10 18:02:23 +0300

    first commit

---
+++ advertisements/filters.py
@@ -0,0 +1,14 @@
+from django_filters import rest_framework as filters, DateFromToRangeFilter
+
+from advertisements.models import Advertisement
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+
+    # TODO: задайте требуемые фильтры
+    created_at = DateFromToRangeFilter(field_name='created_at')
+
+    class Meta:
+        model = Advertisement
+        fields = ['creator', 'status', 'created_at']
